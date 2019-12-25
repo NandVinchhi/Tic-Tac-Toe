6a738aa18bdf2894c688e3fc6e562d24d20d9d28
Nand Vinchhi <58524154+NandVinchhi@users.noreply.github.com>
2019-12-25 21:44:39 +0400

    Update TicTacToe.jl

--- TicTacToe.jl
+++ TicTacToe.jl
@@ -28,10 +28,10 @@ println("A. single player    B. two player.")
 
 choice = readline()
 
-
-
-if choice == "B"
-	println("Player 1 --> X")
+function B()
+        i = 1
+        winstate = "Draw Game."
+        println("Player 1 --> X")
 	println("Player 2 --> Y")
 	println("")
 	
@@ -46,13 +46,13 @@ if choice == "B"
 			p1y = parse(Int64, readline())
 			if board[p1y][p1x] == " "
 			    board[p1y][p1x] = "X"
-			    global i += 1
+			    i += 1
 		    else 
 		    	println("This square is already filled. Please choose another square. ")
 		    end
 			println("")
 			if Game_Over() == 1
-				global winstate = "Player 1 Wins! "
+				winstate = "Player 1 Wins! "
 				break
 			end
         else 
@@ -62,13 +62,13 @@ if choice == "B"
 			p2y = parse(Int64, readline())
 			if board[p2y][p2x] == " "
 			    board[p2y][p2x] = "O"
-			    global i += 1
+			    i += 1
 		    else 
 		    	println("This square is already filled. Please choose another square. ")
 		    end
 			println("")
 			if Game_Over() == 2
-				global winstate = "Player 2 Wins! "
+				winstate = "Player 2 Wins! "
 				break
 			end
 		end
@@ -78,9 +78,11 @@ if choice == "B"
 	println("")
 	println("GaMe oVeR!")
 	println(winstate)
+        
+end
 
-elseif choice == "A"
-	println("Player 1 --> X")
+function A()
+        println("Player 1 --> X")
 	println("Player 2 --> Y")
 	println("")
 	
@@ -95,13 +97,13 @@ elseif choice == "A"
 			p1y = parse(Int64, readline())
 			if board[p1y][p1x] == " "
 			    board[p1y][p1x] = "X"
-			    global i += 1
-		    else 
-		    	println("This square is already filled. Please choose another square. ")
-		    end
+			    i += 1
+		        else 
+		    	    println("This square is already filled. Please choose another square. ")
+		        end
 			println("")
 			if Game_Over() == 1
-				global winstate = "Congratulations, You Win!"
+			        winstate = "Congratulations, You Win!"
 				break
 			end
         else 
@@ -111,111 +113,111 @@ elseif choice == "A"
 			println("")
 			if ((board[1][1] == "X" && board[1][2] == "X") || (board[1][1] == "O" && board[1][2] == "O")) && board[1][3] == " "
 				board[1][3] = "O"
-				global i += 1#
+			        i += 1#
 			elseif ((board[1][1] == "X" && board[1][3] == "X") || (board[1][1] == "O" && board[1][3] == "O")) && board[1][2] == " "
 				board[1][2] = "O"
-				global i += 1#
-		    elseif ((board[1][3] == "X" && board[1][2] == "X") || (board[1][3] == "O" && board[1][2] == "O")) && board[1][1] == " "
+			        i += 1#
+		        elseif ((board[1][3] == "X" && board[1][2] == "X") || (board[1][3] == "O" && board[1][2] == "O")) && board[1][1] == " "
 				board[1][1] = "O"
-				global i += 1#
+				i += 1#
 			elseif ((board[2][1] == "X" && board[2][2] == "X") || (board[2][1] == "O" && board[2][2] == "O")) && board[2][3] == " "
 				board[2][3] = "O"
-				global i += 1#
+				i += 1#
 			elseif ((board[2][1] == "X" && board[2][3] == "X") || (board[2][1] == "O" && board[2][3] == "O")) && board[2][2] == " "
 				board[2][2] = "O"
-				global i += 1#
+				i += 1#
 			elseif ((board[2][2] == "X" && board[2][3] == "X") || (board[2][2] == "O" && board[2][3] == "O")) && board[2][1] == " "
 				board[2][1] = "O"
-				global i += 1#
+				i += 1#
 			elseif ((board[3][1] == "X" && board[3][2] == "X") || (board[3][1] == "O" && board[3][2] == "O")) && board[3][3] == " "
 				board[3][3] = "O"
-				global i += 1#
+				i += 1#
 			elseif ((board[3][1] == "X" && board[3][3] == "X") || (board[3][1] == "O" && board[3][3] == "O")) && board[3][2] == " "
 				board[3][2] = "O"
-				global i += 1#
+				i += 1#
 			elseif ((board[3][2] == "X" && board[3][3] == "X") || (board[3][2] == "O" && board[3][3] == "O")) && board[3][1] == " "
 				board[3][1] = "O"
-				global i += 1#
+				i += 1#
 			elseif ((board[1][1] == "X" && board[2][1] == "X") || (board[1][1] == "O" && board[2][1] == "O")) && board[3][1] == " "
 				board[3][1] = "O"
-				global i += 1#
+				i += 1#
 			elseif ((board[1][1] == "X" && board[3][1] == "X") || (board[1][1] == "O" && board[3][1] == "O")) && board[2][1] == " "
 				board[2][1] = "O"
-				global i += 1#
+				i += 1#
 		    elseif ((board[3][1] == "X" && board[2][1] == "X") || (board[3][1] == "O" && board[2][1] == "O")) && board[1][1] == " "
 				board[1][1] = "O"
-				global i += 1#
+				i += 1#
 			elseif ((board[1][2] == "X" && board[2][2] == "X") || (board[1][2] == "O" && board[2][2] == "O")) && board[3][2] == " "
 				board[3][2] = "O"
-				global i += 1#
+				i += 1#
 			elseif ((board[1][2] == "X" && board[3][2] == "X") || (board[1][2] == "O" && board[3][2] == "O")) && board[2][2] == " "
 				board[2][2] = "O"
-				global i += 1#
+				i += 1#
 			elseif ((board[2][2] == "X" && board[3][2] == "X") || (board[2][2] == "O" && board[3][2] == "O")) && board[1][2] == " "
 				board[1][2] = "O"
-				global i += 1#
+				i += 1#
 			elseif ((board[1][3] == "X" && board[2][3] == "X") || (board[1][3] == "O" && board[2][3] == "O")) && board[3][3] == " "
 				board[3][3] = "O"
-				global i += 1#
+				i += 1#
 			elseif ((board[1][3] == "X" && board[3][3] == "X") || (board[1][3] == "O" && board[3][3] == "O")) && board[2][3] == " "
 				board[2][3] = "O"
-				global i += 1#
+				i += 1#
 			elseif ((board[2][3] == "X" && board[3][3] == "X") || (board[2][3] == "O" && board[3][3] == "O")) && board[1][3] == " "
 				board[1][3] = "O"
-				global i += 1#
+				i += 1#
 			elseif ((board[1][1] == "X" && board[2][2] == "X") || (board[1][1] == "O" && board[2][2] == "O")) && board[3][3] == " "
 				board[3][3] = "O"
-				global i += 1#
+				i += 1#
 			elseif ((board[1][1] == "X" && board[3][3] == "X") || (board[1][1] == "O" && board[3][3] == "O")) && board[2][2] == " "
 				board[2][2] = "O"
-				global i += 1#
+				i += 1#
 			elseif ((board[2][2] == "X" && board[3][3] == "X") || (board[2][2] == "O" && board[3][3] == "O")) && board[1][1] == " "
 				board[1][1] = "O"
-				global i += 1#
+				i += 1#
 			elseif ((board[1][3] == "X" && board[2][2] == "X") || (board[1][3] == "O" && board[2][2] == "O")) && board[3][1] == " "
 				board[3][1] = "O"
-				global i += 1#
+				i += 1#
 			elseif ((board[1][3] == "X" && board[3][1] == "X") || (board[1][3] == "O" && board[3][1] == "O")) && board[2][2] == " "
 				board[2][2] = "O"
-				global i += 1#
+				i += 1#
 			elseif ((board[2][2] == "X" && board[3][1] == "X") || (board[2][2] == "O" && board[3][1] == "O")) && board[1][3] == " "
 				board[1][3] = "O"
-				global i += 1#
+				i += 1#
 
 			else
 				if board[1][1] == " "
 					board[1][1] = "O"
-					global i += 1
+					i += 1
 				elseif board[1][2] == " "
 					board[1][2] = "O"
-					global i += 1
+					i += 1
 				elseif board[1][3] == " "
 					board[1][3] = "O"
-					global i += 1
+					i += 1
 				elseif board[2][1] == " "
 					board[2][1] = "O"
-					global i += 1
+					i += 1
 				elseif board[2][2] == " "
 					board[2][2] = "O"
-					global i += 1
+					i += 1
 				elseif board[2][3] == " "
 					board[2][3] = "O"
-					global i += 1
+					i += 1
 				elseif board[3][1] == " "
 					board[3][1] = "O"
-					global i += 1
+					i += 1
 				elseif board[3][2] == " "
 					board[3][2] = "O"
-					global i += 1
+					i += 1
 				elseif board[3][3] == " "
 					board[3][3] = "O"
-					global i += 1
+					i += 1
 				end
 			end
 			println("The Computer has made a move.")
 			println("")
 			if Game_Over() == 2
-				global winstate = "You Lose, Better luck next time!!"
+				winstate = "You Lose, Better luck next time!!"
 				break
 			end
 		end
@@ -225,7 +227,18 @@ elseif choice == "A"
 	println("")
 	println("GaMe oVeR!")
 	println(winstate)
+end
 
+if choice == "B"
+        B()
+        println("")
+	println("Press Q to quit and any other key to play again. ")
+	choice2 = readline()
+	if choice2 == "Q"
+		break 
+	end
+elseif choice == "A"
+	A()
 	println("")
 	println("Press Q to quit and any other key to play again. ")
 	choice2 = readline()
